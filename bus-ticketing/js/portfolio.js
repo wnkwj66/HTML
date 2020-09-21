483c33089409b81a30a428788e2a1bd264dafbb8
kwj <wnkwj66@naver.com>
2020-09-21 17:40:31 +0900

    메인페이지 - 캘린더

--- bus-ticketing/js/portfolio.js
+++ bus-ticketing/js/portfolio.js
@@ -56,4 +56,28 @@ $(function(){
 			$('.one-way').removeClass('active')
 		})
 	})
+	// 달력
+	$( "#calender" ).datepicker({
+        showOn: "both", 
+        buttonImage: "../css/img/ico_calender.png", 
+        buttonImageOnly: true,
+        //날짜 선택 시 데이터 표기 방법 ex) yy.mm.dd = 2020.09.21
+        dateFormat: "yy. m. d. D", // 2020.9.21.월
+        //한글화
+        dayNames: [ "일요일","월요일","화요일","수요일","목요일","금요일","토요일" ],
+        dayNamesShort: [ "일","월","화","수","목","금","토" ],
+	    dayNamesMin: [ "일","월","화","수","목","금","토" ],
+        monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'],
+        monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+        closeText: "닫기",
+        prevText: "이전달",
+        nextText: "다음달",
+        currentText: "오늘",
+        weekHeader: "주",
+        firstDay: 0,
+        isRTL: false,
+        showMonthAfterYear: true,
+		yearSuffix: ".",
+	}).datepicker('setDate', new Date());
+	$('.ui-datepicker ').css( {"margin":"15px 0 0 -15px"},{})
 })
